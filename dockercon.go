9a63311c4fd52dd625dc981123ed9085bec81af7
Drew Erny <drew.erny@docker.com>
2017-03-30 15:52:15 -0700

    gave gordon a greeting. don't deploy this version!

--- dockercon.go
+++ dockercon.go
@@ -6,18 +6,35 @@ import (
 	"net/http"
 )
 
-const Version string = "2.0"
+const (
+	Version string = "2.1"
+	// TODO(dperny) give gordon a greeting!
+	GordonGreeting string = ""
+)
 
 // version 1.0
 func main() {
 	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		// TODO(dperny) don't let any miscreants deploy without gordon's greeting!
+		if len(GordonGreeting) == 0 {
+			// TODO(NOT SCRIPT)(dperny) write an error handler in v2.0 that logs
+			// errors before returning them
+			http.Error(w, "forgot to give gordon a greeting!", http.StatusInternalServerError)
+			return
+		}
 		log.Printf("Got a healthcheck!")
 		fmt.Fprintln(w, "Healthy!")
 	})
 
+	http.HandleFunc("/gordon", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Got a request for gordon!")
+		fmt.Fprintf(w, "gordon says %v!\n", GordonGreeting)
+	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
-			http.Error(w, fmt.Sprintf("%v not found!", r.URL.Path), http.StatusNotFound)
+			notFound := fmt.Sprintf("%v not found!", r.URL.Path)
+			log.Printf("Error! %v", notFound)
+			http.Error(w, notFound, http.StatusNotFound)
 			return
 		}
 		log.Printf("Got a request!")
